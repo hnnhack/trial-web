8e5b81bd458f2519966145ced5e895cc40241f63
niyas <hojaniyazhan@yahoo.com>
2022-06-25 12:46:10 +0200

    Fotapijt changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welkom naar Fotopijt!</h1>
+        <h1>Welkom naar Fotapijt!</h1>
         <h3>Onze website gaat gauw active zijn.</h3>
         <p>
           Vraag stellen:<br />
